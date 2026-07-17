4a1d97d069d4b905b64d3747884b2b556173b6fd
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2026-07-17 13:37:22 -0400

    finish first set of plot functions

---
+++ 03_plots.R
@@ -0,0 +1,156 @@
+plot_sensitivity <- function(
+    results,
+    x ,
+    xlab,
+    title = NULL,
+    cols = c("black","steelblue","firebrick")
+){
+    matplot(
+        x = results[[x]],
+        y= as.matrix(results[, c("Global","FHNY","LMDC")]),
+        type = "b",
+        pch = 19,
+        lty = 1,
+        cex = 1,
+        lwd = 2,
+        cex.lab = 1.2,
+        cex.axis = 1,
+        col = cols,
+        xlab = xlab,
+        ylab = "Number of core OTUs",
+        main = title
+    )
+    legend(
+        "topright",
+        legend = c("Global","FHNY","LMDC"),
+        col = cols,
+        lty = 1,
+        pch = 19,
+        bty = "n"
+    )
+    invisible(NULL)
+}
+
+
+plot_depth <- function(results, title = NULL){
+    plot_sensitivity(results, x= "Depth", xlab="Rarefaction depth", title = title)
+}
+
+plot_rarefactions <- function(results, title = NULL){
+    plot_sensitivity(results, x= "Rarefactions", xlab="Number of Rarefactions", title = title)
+}
+
+plot_absences <- function(results, title = NULL){
+    plot_sensitivity(results, x= "MaxAbs", xlab="Maximum allowed missing samples", title = title)
+}
+
+plot_thocc <- function(results, title = NULL){
+    plot_sensitivity(results, x= "Threshold", xlab="Occupancy threshold", title = title)
+}
+
+plot_sample_size <- function(results, title = NULL){
+    plot_sensitivity(results, x= "Samples", xlab="Samples per group", title = title)
+}
+
+plot_min_count <- function(results, title = NULL){
+    plot_sensitivity(results, x= "MinCount", xlab="Minimum Number of read per OTUs", title = title)
+}
+
+plot_relative_abundance <- function(results, title = NULL){
+    plot_sensitivity(results, x= "MinRel", xlab="Minimum relative abundance", title = title)
+}
+
+# plot_depth(depth_test)
+# 
+# plot_rarefactions(rarefaction_test)
+# 
+# plot_absences(absence_test)
+# 
+# plot_thocc(occ_threshold_test)
+# 
+# plot_sample_size(samplesize_test)
+# 
+# plot_min_count(count_test)
+# 
+# plot_relative_abundance(relab_test)
+
+
+plot_validation <- function(
+    depth_test,
+    rarefaction_test,
+    absence_test,
+    occ_threshold_test,
+    samplesize_test,
+    count_test,
+    relab_test
+){
+
+    oldpar <- par(no.readonly = TRUE)
+    on.exit(par(oldpar))
+
+    par(mfrow = c(3, 3), mar = c(4,4,2,1))
+
+    plot_depth(depth_test)
+    plot_rarefactions(rarefaction_test)
+    plot_absences(absence_test)
+    plot_thocc(occ_threshold_test)
+    plot_sample_size(samplesize_test)
+    plot_min_count(count_test)
+    plot_relative_abundance(relab_test)
+}
+
+plot_validation(
+    depth_test,
+    rarefaction_test,
+    absence_test,
+    occ_threshold_test,
+    samplesize_test,
+    count_test,
+    relab_test
+)
+
+# plot_core_summary() => barplot(table(core$Classification))
+
+# plot_upset() — overlap of core OTUs among groups.
+# plot_taxonomy() — taxonomic composition of the global and group-specific cores.
+# plot_heatmap() — occupancy of core taxa across groups.
+# plot_occupancy() — occupancy distributions.
+
+
+
+# sample_richness <- colSums(stratified$otu > 0)
+# mean_richness <- tapply(
+#     sample_richness,
+#     stratified$meta$city,
+#     mean
+# )
+# sd_richness <- tapply(
+#     sample_richness,
+#     stratified$meta$city,
+#     sd
+# )
+# data.frame(
+#     City = names(mean_richness),
+#     Mean = round(mean_richness, 1),
+#     SD = round(sd_richness, 1),
+#     Total_OTUs = total_detected[names(mean_richness)]
+# )
+# 
+# test_abs$FHNY_per_sample <- test_abs$FHNY / mean_richness["FHNY"]
+# test_abs$LMDC_per_sample <- test_abs$LMDC / mean_richness["LMDC"]
+# 
+# text(
+#     x = 5,
+#     y = test_abs$FHNY[nrow(test_abs)],
+#     labels = sprintf("%.2f%%", test_abs$FHNY_per_sample[nrow(test_abs)]),
+#     col = "red",
+#     pos = 4, xpd = NA
+# )
+# 
+# text(
+#     x = 5,
+#     y = test_abs$LMDC[nrow(test_abs)],
+#     labels = sprintf("%.2f%%", test_abs$LMDC_per_sample[nrow(test_abs)]),
+#     col = "green4",
+#     pos = 4, xpd = NA
+# )
